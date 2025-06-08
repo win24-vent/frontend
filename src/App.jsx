8b6be55b1d6287daa9210d6d5bacaabc4a4b3967
malson89s <malin.sonneblom@hotmail.se>
2025-06-08 21:41:34 +0200

    fixad import stavning

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import EventPage from './assets/pages/EventPage'
-import EventDetailsPage from './assets/pages/EventDetailsPage'
-import BookingEventPage from './assets/pages/BookingEventPage'
+import EventPage from './assets/Pages/EventPage'
+import EventDetailsPage from './assets/Pages/EventDetailsPage'
+import BookingEventPage from './assets/Pages/BookingEventPage'
 import PortalLayout from './assets/layouts/PortalLayout'
 
 function App() {
